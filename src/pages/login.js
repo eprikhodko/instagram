2367d2c2ccc073c00eb1fa4c29c344f1d39258d7
eprikhodko <eprikhodkodev@gmail.com>
2021-04-28 12:38:15 +0300

    Add basic user authentication via log in page

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from "react"
+import {firebase} from "../lib/firebase"
 import {Link} from "react-router-dom"
 import * as ROUTES from "../constants/routes"
 import "../styles/login.css"
@@ -12,16 +13,58 @@ const Login = () => {
 
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
-    const [error, setError] = useState("")
 
+    const [error, setError] = useState("")
     const isInvalid = password === "" || email === ""
 
-    return(
+    // user actions
+    // happy & sad scenarios
+    
+    // what happens when a user clicks login? -> firebase
+    // a async function that can handle login
+    // handle a succesful login with 
+    // await firebase.auth().signInWithEmailAndPassword(emailAddress, password);
+    
+    // wrap the await function call to firebase in a try/catch
+    // error: catch(error)
+    // setError(error.message)
+    
+    // extra learnings: test.com
+    // handle the email address validation client side
+    // removes a network call!
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+
+        async function handleLogin() {
+            await firebase.auth().signInWithEmailAndPassword(email, password)
+            .then((userCredential) => {
+              // Signed in
+              console.log("signed in")
+              const user = userCredential.user
+              // ...
+            })
+            .catch((error) => {
+              const errorCode = error.code
+              const errorMessage = error.message
+            // save error message in state
+              setError(errorMessage)
+              console.log(errorCode)
+            //   console.log(errorMessage)
+            })
+        }
+        handleLogin(email, password)
+        console.log(error)
+    }
+
+
+
+    return (
         <div className="container">
             <div className="container-login">
                 <img className ="logo" src="./images/logo.png" alt="Instagram logo" />
 
-                <form method="POST" className="container-form">
+                <form method="POST" className="container-form" onSubmit={handleSubmit}>
                     <input 
                         aria-label="Enter your email address"
                         className="input-login"
@@ -49,7 +92,7 @@ const Login = () => {
             </div>
             <div className="container-login container-signup">
                 <p>
-                    Don't have an account?{' '}
+                    Don't have an account?{" "}
                     <Link to={ROUTES.SIGN_UP} className="font-bold link link-signup">
                         Sign up
                     </Link>
