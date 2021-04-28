2a92a098608edc06e448abc5ed2e1defd0f0c021
eprikhodko <eprikhodkodev@gmail.com>
2021-04-28 10:19:35 +0300

    Add basic log in form validation

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,14 +1,20 @@
-import React, { useEffect } from "react"
+import React, {useEffect, useState} from "react"
 import {Link} from "react-router-dom"
 import * as ROUTES from "../constants/routes"
 import "../styles/login.css"
 
 const Login = () => {
-    
+
     // set page title
     useEffect(() => {
         document.title = "Login - Instagram"
     },[])
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [error, setError] = useState("")
+
+    const isInvalid = password === "" || email === ""
 
     return(
         <div className="container">
@@ -21,16 +27,25 @@ const Login = () => {
                         className="input-login"
                         type="text"
                         placeholder="Email address"
+                        value={email}
+                        onChange={(event) => setEmail(event.target.value)}
                     />
                     <input 
                         aria-label="Enter your password"
                         className="input-login"
                         type="password"
                         placeholder="Password"
+                        value={password}
+                        onChange={(event) => setPassword(event.target.value)}
                     />
                     <button 
                         type="submit"
                         className="button-login font-bold"
+                        disabled={!isInvalid ? "" : "disabled"}
+                        style={{
+                            opacity: !isInvalid ? "100%" : "50%",
+                            cursor: !isInvalid ? "" : "not-allowed"
+                        }}
                     >
                         Log In
                     </button>
